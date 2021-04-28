1caacdea67dbfa3001f71c7757c3484f155d5585
Alberto Farci <tarcisio.albi@gmail.com>
2021-04-28 18:53:47 +0200

    Update chiesa-detail.page.ts

--- src/app/chiesa-detail/chiesa-detail.page.ts
+++ src/app/chiesa-detail/chiesa-detail.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ChieseRomaneService } from '../shared/services/chiese-romane.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-chiesa-detail',
@@ -20,7 +21,12 @@ export class ChiesaDetailPage implements OnInit {
   ngOnInit() {
     const id = this.activeRoute.snapshot.paramMap.get('id');
 
-    this.chiesa = this.chieseRomaneService.getChiesaById(id);
+    this.chiesa = this.chieseRomaneService.getAllChieseAndIntineraries()
+      .pipe(
+        map(result =>
+          result.filter(one => one.id == id)
+        )
+      );
   }
 
   ionViewWillEnter() {
