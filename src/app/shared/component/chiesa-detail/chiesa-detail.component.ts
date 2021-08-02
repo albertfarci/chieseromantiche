658599827c1326406b7436c7a1c5d0eb26a6eabf
Alberto Farci <tarcisio.albi@gmail.com>
2021-08-02 21:02:46 +0200

    added audio play

--- src/app/shared/component/chiesa-detail/chiesa-detail.component.ts
+++ src/app/shared/component/chiesa-detail/chiesa-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, NgZone, OnChanges } from '@angular/core';
+import { Component, Input, NgZone, OnChanges, ViewChild } from '@angular/core';
 import { NativeStorage } from '@ionic-native/native-storage/ngx';
 import { Toast } from '@ionic-native/toast/ngx';
 import { DashboardTemplateService } from 'src/app/dashboard/services/dashboard-template.service';
@@ -6,6 +6,7 @@ import { SLIDES_OPTIONS_CONFIGURATIONS } from '../../models/slideoptions.model';
 import { SocialSharing } from '@ionic-native/social-sharing/ngx';
 import { SQLite, SQLiteObject } from '@ionic-native/sqlite/ngx';
 import { StorageService } from '../../services/storage.service';
+import { IonRange } from '@ionic/angular';
 @Component({
   selector: 'app-chiesa-detail-component',
   templateUrl: './chiesa-detail.component.html',
@@ -22,6 +23,19 @@ export class ChiesaDetailComponent {
 
   slideOpts = SLIDES_OPTIONS_CONFIGURATIONS.get('photo-gallery')
 
+  // ...
+@ViewChild("range", { static: false }) range: IonRange;
+
+// ...
+media: any;
+
+// ...
+audioPlayer = null;
+audioDuration = null;
+audioProgress = null;
+audioTimer = null;
+audioIsPaused = false;
+nowPlayingAudioIndex = null;
   
   constructor(
     private storage: StorageService,
@@ -71,4 +85,74 @@ export class ChiesaDetailComponent {
   SendEmail() {
     this.socialSharing.shareViaEmail('text', 'subject', ['email@address.com'])
   }
+
+
+playPlauseAudio(index: number, file: string) {
+  if (index === this.nowPlayingAudioIndex) {
+    // update current player
+    if (this.audioIsPaused) {
+      // resume
+      this.audioIsPaused = false;
+      if (this.audioPlayer) this.audioPlayer.play();
+    } else {
+      // pause
+      this.audioIsPaused = true;
+      if (this.audioPlayer) this.audioPlayer.pause();
+    }
+  } else {
+    if (this.audioPlayer) {
+      // stop any existing audio
+      this.destroyAudio();
+    }
+    // create and play new track
+    this.nowPlayingAudioIndex = index;
+    this.audioIsPaused = false;
+    this.audioPlayer = new Audio(file);
+    this.audioPlayer.play().then(() => {
+      this.audioDuration = this.audioPlayer.duration;
+      this.audioTimer = setInterval(() => {
+        this.audioProgress = this.audioPlayer.currentTime;
+        this.audioPlayer.onended = () => {
+          this.destroyAudio();
+        };
+      }, 100);
+    });
+  }
+}
+
+seek(index: number, event: any) {
+  if (index == this.nowPlayingAudioIndex) {
+    let seekTo = event.target.value;
+    this.audioPlayer.currentTime = seekTo;
+    if (this.audioIsPaused == true) {
+      this.audioPlayer.play();
+      this.audioIsPaused = false;
+    }
+  }
+}
+
+pauseWhileSeeking(index: number) {
+  if (index == this.nowPlayingAudioIndex) {
+    this.audioIsPaused = true;
+    this.audioPlayer.pause();
+  }
+}
+
+getAudioProgress(seconds: number) {
+  if (seconds >= 0) {
+    return new Date(seconds * 1000).toISOString().substr(14, 5);
+  }
+}
+
+destroyAudio() {
+  if (this.audioPlayer && !this.audioIsPaused) this.audioPlayer.pause();
+  if (this.audioTimer) clearInterval(this.audioTimer);
+  if (this.range) this.range.value = 0;
+  this.audioPlayer = null;
+  this.audioDuration = null;
+  this.audioProgress = null;
+  this.audioTimer = null;
+  this.audioIsPaused = false;
+  this.nowPlayingAudioIndex = null;
+}
 }
